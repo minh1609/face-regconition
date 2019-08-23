edff66978203e24e5959e44711b6f7f8112e7b18
minh <hoangminh160997@gmail.com>
2019-08-22 22:46:55 -0700

    Updates

---
+++ precache-manifest.18e1c5a4531f3591062e94e35ac5874f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8703b9d67c8d0f6b655c314d1cfcd25f",
+    "url": "/face-regconition/index.html"
+  },
+  {
+    "revision": "559400f90c86dc2d2bb7",
+    "url": "/face-regconition/static/js/2.f275b4c6.chunk.js"
+  },
+  {
+    "revision": "54d12e32939342c7ecb4",
+    "url": "/face-regconition/static/js/main.7ce4ccc6.chunk.js"
+  },
+  {
+    "revision": "9209d34fa91418afa0c3",
+    "url": "/face-regconition/static/js/runtime~main.90e81402.js"
+  }
+]);
